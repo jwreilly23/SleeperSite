4e64af22c5fd70693ea045353db6852e0f4f9109
Jack Reilly <70866001+jwreilly23@users.noreply.github.com>
2023-07-08 00:04:54 -0400

    top 6 standings added
    slow to query...might need to check the call frequency. Also need to add bottom 6, and clean up the None standings

--- league_history/views.py
+++ league_history/views.py
@@ -73,6 +73,16 @@ def get_user_id(username):
         return response["user_id"]
     else:
         return False
+    
+def get_username(user_id):
+    '''
+    Takes a username, returns the user id
+    '''
+    response = requests.get(baseURL+f"/user/{user_id}").json()
+    if response is not None:
+        return response["username"]
+    else:
+        return False
 
 def get_user_leagues(user_id):
     '''
@@ -83,39 +93,57 @@ def get_user_leagues(user_id):
 
     # loop through and get each league name
     leagues = []
-    for league in response:
-        # maybe make this a class? league.Name, league.Id, etc.
-        leagues.append(SleeperLeague(league["name"], league["league_id"], league["avatar"]))
+    for league_raw in response:
+        # initialize class        
+        league = SleeperLeague(league_raw["name"], league_raw["league_id"], league_raw["avatar"], league_raw["total_rosters"])
+
+        # get standings
+        league.set_standings(get_winner_bracket(league.id, league.size))
+
+        leagues.append(league)
 
     return leagues
 
-def get_winner_bracket(league_id):
+def get_winner_bracket(league_id, league_size):
     '''
     Takes a league id, returns a list of [1st, 2nd, 3rd...etc] for top 5 finish
     '''
-    response = requests.get(baseURL+f"/league/{league_id}/winners_bracket").json()
+    # get the playoff brackets
+    winners_raw = requests.get(baseURL+f"/league/{league_id}/winners_bracket").json()
+    # losers_raw = requests.get(baseURL+f"/league/{league_id}/losers_bracket").json()
+
+    # get the league rosters, to relate bracket roster id's to usernames
+    rosters_dict = {}
+    league_rosters_raw = requests.get(baseURL+f"/league/{league_id}/rosters").json()
+    for roster in league_rosters_raw:
+        owner_username = get_username(roster["owner_id"])
+        rosters_dict[roster["roster_id"]] = owner_username
+        # print( str(roster["roster_id"]) + " - " + owner_username)        
 
     # currently only returning top 6
-    final_standings = [None] * 3
+    final_standings = [None] * league_size
 
-    # loop through twice to find the highest round number...accounts for playoffs with any number of rounds
-    final_round = 1
-    for matchup in response:
-        if matchup["r"] > final_round:
-            final_round = matchup["r"]
+    # fill in standings ... winner (w) == null if the round hasn't finished yet
+    for matchup in winners_raw:
+        if "p" in matchup:
+            # check if no winner...meaning no playoffs (edge case leagues like Megalabowl)
+            if matchup["w"] is None:
+                return final_standings
 
-    for matchup in response:
-        # check if final round
-        if matchup["r"] == final_round:
-            # 1st and 2nd
-            if matchup["p"] == 1:
+            # p is place of winner...meaning if p=1, final_standings[p-1] = winner ... f_s[p] = loser
+            final_standings[matchup["p"]-1] = rosters_dict[matchup["w"]]
+            final_standings[matchup["p"]] = rosters_dict[matchup["l"]]            
 
-
-
+    return final_standings
 
 class SleeperLeague:
-    def __init__(self, name, id, avatar):
+    def __init__(self, name, id, avatar, size, standings=None):
         self.name = name
         self.id = id
         self.avatar = avatar
+        self.size = size
+        self.standings = standings
+
+    def set_standings(self, standings):
+        self.standings = standings
 
